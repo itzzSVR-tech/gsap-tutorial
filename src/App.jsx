92a6341ecc6bff5286a3eb121712a8f9c5d40356
itzzSVR-tech <marioplayz1975@gmail.com>
2025-06-01 19:14:28 +0530

    Added Other Sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +14,9 @@ const App = () => {
         <main>
             <Navbar />
             <Hero />
-            <div className="h-dvh bg-black" />
+            <Cocktails />
+            <About />
+            <Art />
         </main>
     );
 };
